13686957494fb5f774281d8d941aafd67c8b1514
NeedToLookAtMyPictures <schmeedainc@gmail.com>
2025-08-02 15:44:43 -0400

    formatting

--- Steam_Search/main.cpp
+++ Steam_Search/main.cpp
@@ -235,7 +235,7 @@ int main()
                 {
                     cout << "\nNo more games to display in this ranking. Would you like to calculate more? [y/n]" << endl;
                     cin >> response;
-                    cout << "\nPlease Wait..." << endl;
+                    cout << "Please Wait..." << endl;
                     if (response == "y")
                     {
 
